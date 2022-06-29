2920c5958b583d2b0f768a0d3c73a8426515199d
oleg <olegarxo15@gmail.com>
2022-06-29 12:18:15 +0300

    Сделал первое домашнее задание

---
+++ homework1/Program.cs
@@ -0,0 +1,32 @@
+﻿void findTrid (string number)
+{
+int truNumbers = Convert.ToInt32(number);    
+    if (truNumbers > 0)
+    {
+
+    
+        if (number.Length >= 3 )
+        {
+            Console.WriteLine(number[2]);
+        }
+        else 
+        {
+            Console.WriteLine("Третьего числа нет");
+        }
+    } 
+    else 
+    {
+        if (number.Length >= 4 )
+        {
+            Console.WriteLine(number[3]);
+        }
+        else 
+        {
+            Console.WriteLine("Третьего числа нет");
+        } 
+    }   
+}
+Console.WriteLine("введите число");
+string number = Console.ReadLine();
+Console.WriteLine(number.Length);
+findTrid(number);
